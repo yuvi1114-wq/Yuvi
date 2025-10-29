fe4f77dd44c9673a483c26ae82846c2d0c641e42
yuvi1114-wq <yuvi181810@gmail.com>
2025-10-29 12:35:49 +0530

    Write a program to calculate simple and compound interest for given principal, rate, and time.

---
+++ day5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+    double principal, rate, time;
+    double simpleInterest, compoundInterest;
+
+ 
+    printf("Enter principal amount: ");
+    scanf("%lf", &principal);
+
+    printf("Enter annual interest rate (in %%): ");
+    scanf("%lf", &rate);
+
+    printf("Enter time in years: ");
+    scanf("%lf", &time);
+
+    simpleInterest = (principal * rate *
+}
