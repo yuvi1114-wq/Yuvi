bf0422857579db7068d8e0cb84e500968086d10e
yuvi1114-wq <yuvi181810@gmail.com>
2025-10-29 11:31:34 +0530

    Add files via upload

---
+++ addof2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1,num2,sum;
+    printf("enter the number:");
+    scanf("%d" ,&num1);
+    printf("enter the num2:");
+    scanf("%d", &num2);
+    sum = num1 +num2;
+    printf("the sum is %d\n",sum);
+    return 0;
+}
